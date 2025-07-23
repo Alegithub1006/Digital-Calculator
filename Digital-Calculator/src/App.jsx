45c7e09648f60ae482fa25406268da0f3290778b
muchu1006 <alejandro0610.gp@gmail.com>
2025-07-23 13:59:08 -0400

    add basic-structure

--- Digital-Calculator/src/App.jsx
+++ Digital-Calculator/src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+/*
+*  2. Calculadora Digital
+ Descripción: Calculadora científica con funcionalidad de historial
+ Tareas Principales:
+ * Operaciones aritméticas básicas,
+ *  funciones de limpiar/resetear,
+ *  soporte decimal
+ Tareas Bonus: Funciones científicas (sin, cos, tan),
+ *  historial de cálculos,
+ *  soporte de teclado
+ *  Stack Tecnológico: React + Vite, Styled
+ Components APIs: Ninguna requerida
+ Advertencia: Maneja casos edge como división por cero y
+ precisión de punto flotante
+* */
 import './App.css'
+import WindowCalculator from "./components/WindowCalculator.jsx";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
+function DigitalCalculator() {
+  return(
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Digital-calculator</h1>
+        <WindowCalculator/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
-export default App
+export default DigitalCalculator
